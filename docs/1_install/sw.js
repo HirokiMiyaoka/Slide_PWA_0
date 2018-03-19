fc31c378452df042c8294d6181eb72a6f4786614
Hiroki <hiroki.miyaoka@gmail.com>
2018-03-19 15:17:17 +0900

    Update sw.js

--- docs/1_install/sw.js
+++ docs/1_install/sw.js
@@ -1,12 +1,8 @@
-let count = { install: 0, activate: 0 };
-
 self.addEventListener( 'install', ( event ) => {
-	console.log( 'SW:', 'install', ++count.install );
+	console.log( 'SW:', 'install' );
 	console.log( location );
-	event.waitUntil( self.skipWaiting() );
 } );
 
 self.addEventListener( 'activate', ( event ) => {
-	console.log( 'SW:', 'activate', ++count.activate );
-	event.waitUntil( self.clients.claim() );
+	console.log( 'SW:', 'activate' );
 } );
