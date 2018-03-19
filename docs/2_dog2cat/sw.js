a9e8d9a5e75eaf11506dd61f190d07451d04d955
Hiroki <hiroki.miyaoka@gmail.com>
2018-03-19 13:18:18 +0900

    Update sw.js

--- docs/2_dog2cat/sw.js
+++ docs/2_dog2cat/sw.js
@@ -3,7 +3,7 @@ const CACHE_VERSION = 'sample_static-v1';
 self.addEventListener( 'install', ( event ) => {
 	console.log( 'SW:', 'install' );
 	event.waitUntil( caches.open( CACHE_VERSION ).then( ( cache ) => {
-		cache.add('./cat.svg');
+		cache.add('/Slide_PWA_0/2_dog2cat/cat.svg');
 	} ) );
 } );
 
@@ -16,6 +16,6 @@ self.addEventListener( 'fetch', ( event ) => {
 	const url = new URL( event.request.url );
 
 	if ( url.origin == location.origin && url.pathname == '/Slide_PWA_0/2_dog2cat/dog.svg' ) {
-		event.respondWith( caches.match( '/cat.svg' ) );
+		event.respondWith( caches.match( '/Slide_PWA_0/2_dog2cat/cat.svg' ) );
 	}
 } );
