cbc5d9d56fcd0d4c124dcab1bb61a750203c6ca2
HirokiMiyaoka <hiroki.miyaoka@gmail.com>
2018-03-19 18:46:57 +0900

    Update.

--- docs/6_update_found/sw.js
+++ docs/6_update_found/sw.js
@@ -7,14 +7,3 @@ self.addEventListener( 'install', ( event ) => {
 self.addEventListener( 'activate', ( event ) => {
 	console.log( 'SW:', 'activate' );
 } );
-
-self.addEventListener( 'fetch', ( event ) => {
-	console.log( 'SW:', 'fetch', event.request.url );
-	const url = new URL( event.request.url );
-
-	if ( url.origin == location.origin && url.pathname == '/Slide_PWA_0/2_dog2cat/dog.svg' ) {
-		event.respondWith( fetch( '/Slide_PWA_0/2_dog2cat/cat.svg' ).then( ( response ) => {
-			return response;
-		} ) );
-	}
-} );
