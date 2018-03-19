6bf73d253f6cd42c8e2ef2b8b9955b05ae92c69a
Hiroki <hiroki.miyaoka@gmail.com>
2018-03-19 14:57:41 +0900

    Update sw.js

--- docs/4_dog2cat_omake/sw.js
+++ docs/4_dog2cat_omake/sw.js
@@ -1,35 +1,20 @@
-const DOMAIN = 'sample_static-v';
-const CACHE_VERSION = 1;
-const CACHE_NAME = DOMAIN + CACHE_VERSION;
-
 self.addEventListener( 'install', ( event ) => {
 	console.log( 'SW:', 'install' );
-	event.waitUntil( caches.open( CACHE_NAME ).then( ( cache ) => {
-		cache.add('/Slide_PWA_0/4_dog2cat_omake/cat.svg');
-	} ) );
+	event.waitUntil( self.skipWaiting() );
 } );
 
 self.addEventListener( 'activate', ( event ) => {
 	console.log( 'SW:', 'activate' );
-	event.waitUntil( caches.keys().then( ( keys ) => {
-		return Promise.all(
-			keys.filter( ( key ) => {
-				return key.indexOf( DOMAIN ) === 0 && key !== CACHE_NAME;
-			} ).map( ( key ) => {
-				console.log( 'Delete cache:', key );
-				return caches.delete( key );
-			} )
-		);
-	} ).then( () => {
-		return self.clients.claim();
-	} ) );
+	event.waitUntil( self.clients.claim() );
 } );
 
 self.addEventListener( 'fetch', ( event ) => {
 	console.log( 'SW:', 'fetch', event.request.url );
 	const url = new URL( event.request.url );
 
-	if ( url.origin == location.origin && url.pathname == '/Slide_PWA_0/4_dog2cat_omake/dog.svg' ) {
-		event.respondWith( caches.match( '/Slide_PWA_0/4_dog2cat_omake/cat.svg' ) );
+	if ( url.origin == location.origin && url.pathname == '/Slide_PWA_0/2_dog2cat/dog.svg' ) {
+		event.respondWith( fetch( '/Slide_PWA_0/2_dog2cat/cat.svg' ).then( ( response ) => {
+			return response;
+		} ) );
 	}
 } );
