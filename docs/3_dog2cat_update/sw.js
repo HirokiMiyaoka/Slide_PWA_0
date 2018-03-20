1da894fbcbbd57a9bedb1837a3ba7c2d0e2c9adb
Hiroki <hiroki.miyaoka@gmail.com>
2018-03-20 15:47:10 +0900

    Update sw.js

--- docs/3_dog2cat_update/sw.js
+++ docs/3_dog2cat_update/sw.js
@@ -12,8 +12,6 @@ self.addEventListener( 'fetch', ( event ) => {
 	const url = new URL( event.request.url );
 
 	if ( url.origin == location.origin && url.pathname == '/Slide_PWA_0/3_dog2cat_update/dog.svg' ) {
-		event.respondWith( fetch( '/Slide_PWA_0/3_dog2cat_update/cat.svg' ).then( ( response ) => {
-			return response;
-		} ) );
+		event.respondWith( fetch( '/Slide_PWA_0/3_dog2cat_update/cat.svg' ) );
 	}
 } );
