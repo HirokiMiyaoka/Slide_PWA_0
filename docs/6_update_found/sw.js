8c1cd838bf2f071f7a2d3ca06176b850c5b1ca05
HirokiMiyaoka <hiroki.miyaoka@gmail.com>
2018-03-20 09:46:46 +0900

    Test.

--- docs/6_update_found/sw.js
+++ docs/6_update_found/sw.js
@@ -1,9 +1,12 @@
-const VERSION = 2;
+const VERSION = 1;
+let count = { install: 0, activate: 0 };
 
 self.addEventListener( 'install', ( event ) => {
-	console.log( 'SW:', 'install', VERSION );
+	console.log( 'SW:', 'install', 'ver:' + VERSION, ++count.install );
+	event.waitUntil( self.clients.claim() );
 } );
 
 self.addEventListener( 'activate', ( event ) => {
-	console.log( 'SW:', 'activate' );
+	console.log( 'SW:', 'activate', ++count.activate );
+	event.waitUntil( self.clients.claim() );
 } );
